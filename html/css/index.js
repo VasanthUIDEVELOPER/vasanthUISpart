7fa01f3b8d04d9176dfe5eb868357d16ff670192
VasanthUIDEVELOPER <vasanth.m@willwaretech.com>
2024-02-13 15:54:27 +0530

    Calculator

--- html/css/index.js
+++ html/css/index.js
@@ -1,21 +1,71 @@
-// let str1="Strive not to be a success";
-// let str2="but rather to be a value";
-// function generatestring(length)
-// {
-//     let str3="Bharathi";
-// let result=" ";
-// const str3length=str3.length;
-// for(let i=0;i<length;i++)
-// {
-//     result +=str3.charAt(Math.floor(Math.random()*str3));
-// }
-// return result;
-// }
-// console.log(generatestring(2));
-// let str3="bharathi";
-// const r= str3[Math.floor(Math.random()* str3.length)];
-// console.log(r);
-let a=40,b=30;
-let max;
-max=a>b?a:b;
-console.log(max)
+document.addEventListener("DOMContentLoaded",function(){
+    let display=document.getElementById("display");
+    let currentvalue="0";
+    document.addEventListener("keydown",function(event)
+    {
+        const key=event.key;
+        if(isDigit(key)){
+            handleDigit(key);
+        }
+            else if(isOperator(key)){
+                handleOperator(key);
+            }
+            else if(key==="Enter" || key==="="){
+                calculateResult();
+            }
+            else if(key==="Escape"){
+                clearDisplay();
+            }
+            else if(key==="Backspace"){
+                backspace();
+            }        
+    });
+    function isDigit(key){
+        return /^\d$/.test(key);
+    }
+    function isOperator(key){
+        return /^[\+\-\*\/]$/.test(key);
+    }
+    function handleDigit(digit){
+        if(currentvalue==="0"){
+            currentvalue=digit;
+        }
+        else{
+            currentvalue +=digit;
+        }
+        updateDisplay();
+    }
+    function handleOperator(operator)
+    {
+        if(currentvalue !=="0" && !isOperator(currentvalue.charAt(currentvalue.length-1))){
+            currentvalue +=operator;
+            updateDisplay();
+        }
+    }
+    function calculateResult(){
+        try{
+            currentvalue=eval(currentvalue).toString();
+            updateDisplay();
+        }
+        catch (error){
+            display.textContent="Error";
+            currentvalue="0";
+        }
+    }
+    function clearDisplay(){
+        currentvalue="0";
+        updateDisplay();
+    }
+    function backspace(){
+        if(currentvalue.length>1){
+            currentvalue=currentvalue.slice(0,-1);
+        }
+        else{
+            currentvalue="0";
+        }
+        updateDisplay();
+    }
+    function updateDisplay(){
+        display.textContent=currentvalue;
+    }
+});
